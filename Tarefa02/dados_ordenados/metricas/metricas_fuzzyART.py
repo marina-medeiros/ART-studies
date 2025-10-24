bf697000fdcc60eece3e26831bf54a067d7674d1
marina-medeiros <marinamaleite@gmail.com>
2025-10-24 13:42:53 -0300

    unificando arquivos para colocar no collab

--- Tarefa02/dados_ordenados/metricas/metricas_fuzzyART.py
+++ Tarefa02/dados_ordenados/metricas/metricas_fuzzyART.py
@@ -37,42 +37,51 @@ def train_fuzzyART(X_train_subset, y_train_subset, X_test_subset, y_test_subset)
 
 
 def generate_acc_matrix_fuzzyART(num_tasks, X_train_sorted, y_train_sorted, X_test_sorted, y_test_sorted, images):
-  train_subsets = []
-  test_subsets = []
+    train_subsets = []
+    test_subsets = []
 
-  acc_matrix = [[0 for _ in range(num_tasks)] for _ in range(num_tasks)]
+    acc_matrix = [[0 for _ in range(num_tasks)] for _ in range(num_tasks)]
 
-  for i in range(num_tasks):
-    for j in range(num_tasks):
-        # Classes até a i-ésima (inclusive)
-        train_classes = torch.arange(0, i + 1)
+    for i in range(num_tasks):
+        for j in range(num_tasks):
+            # Classes até a i-ésima (inclusive)
+            train_classes = torch.arange(0, i + 1)
 
-        # Máscara de seleção para treino: todas as classes <= i
-        mask_train = torch.isin(y_train_sorted, train_classes)
-        X_train_subset = X_train_sorted[mask_train]
-        y_train_subset = y_train_sorted[mask_train]
+            # Máscara de seleção para treino: todas as classes <= i
+            mask_train = torch.isin(y_train_sorted, train_classes)
+            X_train_subset = X_train_sorted[mask_train]
+            y_train_subset = y_train_sorted[mask_train]
 
-        # Máscara de seleção para teste: apenas a classe j
-        mask_test = (y_test_sorted == j)
-        X_test_subset = X_test_sorted[mask_test]
-        y_test_subset = y_test_sorted[mask_test]
+            # Máscara de seleção para teste: apenas a classe j
+            mask_test = (y_test_sorted == j)
+            X_test_subset = X_test_sorted[mask_test]
+            y_test_subset = y_test_sorted[mask_test]
 
-        # Armazena os subconjuntos (opcional)
-        train_subsets.append((X_train_subset, y_train_subset))
-        test_subsets.append((X_test_subset, y_test_subset))
+            # Armazena os subconjuntos (opcional)
+            train_subsets.append((X_train_subset, y_train_subset))
+            test_subsets.append((X_test_subset, y_test_subset))
 
-        if(images):
-            acc_matrix[i][j] = train_fuzzyART_images(
-                                X_train_subset,
-                                y_train_subset,  # não é usado na função
-                                X_test_subset,
-                                y_test_subset, 
-                                16 * 16)
-        else:
-            acc_matrix[i][j] = train_fuzzyART(
-                                X_train_subset,
-                                y_train_subset,  # não é usado na função
-                                X_test_subset,
-                                y_test_subset)
+            print(f"Classes de treino: {torch.unique(y_train_subset)}")
+            print(f"Classes de teste: {torch.unique(y_test_subset)}")
+            print(f"Tarefa {i}, Teste classe {j}, Tamanho do treino: {len(X_train_subset)}, Tamanho do teste: {len(X_test_subset)}")
+
+            if len(X_train_subset) == 0 or len(X_test_subset) == 0:
+                print("Problema: subconjunto de dados vazio")
+                acc_matrix[i][j] = 0.0
+                continue
+
+            if(images):
+                acc_matrix[i][j] = train_fuzzyART_images(
+                                    X_train_subset,
+                                    y_train_subset,  # não é usado na função
+                                    X_test_subset,
+                                    y_test_subset, 
+                                    16 * 16)
+            else:
+                acc_matrix[i][j] = train_fuzzyART(
+                                    X_train_subset,
+                                    y_train_subset,  # não é usado na função
+                                    X_test_subset,
+                                    y_test_subset)
             
     return acc_matrix
--- Tarefa02/dados_ordenados/metricas/metricas_fuzzyARTMAP.py
+++ Tarefa02/dados_ordenados/metricas/metricas_fuzzyARTMAP.py
@@ -36,41 +36,41 @@ def train_fuzzyARTMAP(X_train_subset, y_train_subset, X_test_subset, y_test_subs
 
 
 def generate_acc_matrix_fuzzyARTMAP(num_tasks, X_train_sorted, y_train_sorted, X_test_sorted, y_test_sorted, images):
-  train_subsets = []
-  test_subsets = []
+    train_subsets = []
+    test_subsets = []
 
-  acc_matrix = [[0 for _ in range(num_tasks)] for _ in range(num_tasks)]
+    acc_matrix = [[0 for _ in range(num_tasks)] for _ in range(num_tasks)]
 
-  for i in range(num_tasks):
-    for j in range(num_tasks):
-        # Classes até a i-ésima (inclusive)
-        train_classes = torch.arange(0, i + 1)
+    for i in range(num_tasks):
+        for j in range(num_tasks):
+            # Classes até a i-ésima (inclusive)
+            train_classes = torch.arange(0, i + 1)
 
-        # Máscara de seleção para treino: todas as classes <= i
-        mask_train = torch.isin(y_train_sorted, train_classes)
-        X_train_subset = X_train_sorted[mask_train]
-        y_train_subset = y_train_sorted[mask_train]
+            # Máscara de seleção para treino: todas as classes <= i
+            mask_train = torch.isin(y_train_sorted, train_classes)
+            X_train_subset = X_train_sorted[mask_train]
+            y_train_subset = y_train_sorted[mask_train]
 
-        # Máscara de seleção para teste: apenas a classe j
-        mask_test = (y_test_sorted == j)
-        X_test_subset = X_test_sorted[mask_test]
-        y_test_subset = y_test_sorted[mask_test]
+            # Máscara de seleção para teste: apenas a classe j
+            mask_test = (y_test_sorted == j)
+            X_test_subset = X_test_sorted[mask_test]
+            y_test_subset = y_test_sorted[mask_test]
 
-        # Armazena os subconjuntos (opcional)
-        train_subsets.append((X_train_subset, y_train_subset))
-        test_subsets.append((X_test_subset, y_test_subset))
+            # Armazena os subconjuntos (opcional)
+            train_subsets.append((X_train_subset, y_train_subset))
+            test_subsets.append((X_test_subset, y_test_subset))
 
-        if(images):
-            acc_matrix[i][j] = train_fuzzyARTMAP_images(
-                                X_train_subset,
-                                y_train_subset,  # não é usado na função
-                                X_test_subset,
-                                y_test_subset, 
-                                16 * 16)
-        else:
-            acc_matrix[i][j] = train_fuzzyARTMAP(
-                                X_train_subset,
-                                y_train_subset,  # não é usado na função
-                                X_test_subset,
-                                y_test_subset)
+            if(images):
+                acc_matrix[i][j] = train_fuzzyARTMAP_images(
+                                    X_train_subset,
+                                    y_train_subset,  # não é usado na função
+                                    X_test_subset,
+                                    y_test_subset, 
+                                    16 * 16)
+            else:
+                acc_matrix[i][j] = train_fuzzyARTMAP(
+                                    X_train_subset,
+                                    y_train_subset,  # não é usado na função
+                                    X_test_subset,
+                                    y_test_subset)
     return acc_matrix
